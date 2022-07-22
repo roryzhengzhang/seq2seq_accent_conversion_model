5c59fedabd1b5e800c4423e6aad293eaf948f028
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-22 16:05:03 -0700

    init

--- prepare_data.py
+++ prepare_data.py
@@ -1,18 +1,28 @@
+import os
 import glob
-import re
-from pathlib import Path
+from random import shuffle
 
-files = glob.glob('audio_data_16k/*.wav')
+pairs = []
+refer_name = 'bdl'
+for f in glob.glob(f"/Users/test/Documents/GitHub/tacotron2/data_wav_16k/{refer_name}/*.wav"):
+    filename = f.split('/')[-1]
+    audioname = filename[len(refer_name)+1:-4]
 
-accent_mapping = {
-    "nora": "GB"
-}
+    for speaker in ['clb', 'EBVS', 'ERMS', 'MBMPS', 'NJS', 'rms', 'slt', 'bdl']:
+        tar_audio_file = f'/Users/test/Documents/GitHub/tacotron2/data_wav_16k/{speaker}/{speaker}_{audioname}.wav'
+        speaker_emb = f'/Users/test/Documents/GitHub/tacotron2/speaker_emb/{speaker}/{speaker}_{audioname}.npy'
+        accent_emb = f'/Users/test/Documents/GitHub/tacotron2/accent_emb/{speaker}/{speaker}_{audioname}.npy'
 
-with open("train_files.txt", 'w') as out: 
-    for f in files:
-        p = Path(f)
-        path = Path(*p.parts[1:])
-        speaker = re.match(r'([A-Za-z]+)', Path(f).stem)
-        speaker = speaker.groups(0)[0]
-        accent = accent_mapping[speaker]
-        out.write(f"{path},{speaker},{accent}\n")
+        # print(f"{tar_audio_file}, {speaker_emb}, {accent_emb}")
+
+        if os.path.isfile(tar_audio_file):
+            pairs.append([f, tar_audio_file, speaker_emb, accent_emb])
+    
+shuffle(pairs)
+    
+with open('train_pairs.txt', 'w') as train_file, open('val_pairs.txt', 'w') as val_file:
+    for i, pair in enumerate(pairs):
+        if i < len(pairs)-50:
+            train_file.write(f"{pair[0]},{pair[1]},{pair[2]},{pair[3]}\n")
+        else:
+            val_file.write(f"{pair[0]},{pair[1]},{pair[2]},{pair[3]}\n")
