780df4047a8a52aac1841a733255d1c08d49cbe2
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-29 01:46:03 -0700

    update prepare_data_new.py

--- prepare_data_new.py
+++ prepare_data_new.py
@@ -7,17 +7,16 @@ from utils.common import hparams
 from utils.common.data_utils import PPGMelLoader
 
 
+with open('filelist.txt', 'w') as f:
+    audiofiles = []
+    for audio in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data_wav_16k/*/*.wav')):
+        audiofiles.append(audio)
 
-# with open('filelist.txt', 'w') as f:
-#     audiofiles = []
-#     for audio in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data_wav_16k/*/*.wav')):
-#         audiofiles.append(audio)
+    random.seed(1234)
+    random.shuffle(audiofiles)
 
-#     random.seed(1234)
-#     random.shuffle(audiofiles)
-
-#     for audio in audiofiles:
-#         f.write(audio+'\n')
+    for audio in audiofiles:
+        f.write(audio+'\n')
 
 
 hparams = create_hparams()
