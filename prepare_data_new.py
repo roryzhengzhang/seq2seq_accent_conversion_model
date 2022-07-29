adb09eb6d565df991105812f4dfc292e795b9464
roryzhengzhang <40183715+roryzhengzhang@users.noreply.github.com>
2022-07-28 23:30:09 -0700

    uplodate prepare_data_new

---
+++ prepare_data_new.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import random
+from venv import create
+from hparams import create_hparams
+from utils.common import hparams
+from utils.common.data_utils import PPGMelLoader
+
+
+
+# with open('filelist.txt', 'w') as f:
+#     audiofiles = []
+#     for audio in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data_wav_16k/*/*.wav')):
+#         audiofiles.append(audio)
+
+#     random.seed(1234)
+#     random.shuffle(audiofiles)
+
+#     for audio in audiofiles:
+#         f.write(audio+'\n')
+
+
+hparams = create_hparams()
+loader = PPGMelLoader( hparams, data_utterance_paths='filelist.txt')
